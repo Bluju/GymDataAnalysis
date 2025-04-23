4e122d639a5ca158ef035e2423abd72b726c8789
Julian <sahaguj@pnw.edu>
2025-04-22 20:36:58 -0500

    Update FinalProjectGymData.R

--- FinalProjectGymData.R
+++ FinalProjectGymData.R
@@ -1,5 +1,9 @@
 ### Julian Sahagun and Peter Bizoukas
 ### STAT 43000
 ### Analysis of Gym Dataset (Final Project)
-data = read.csv("C:\\Users\\PGBiz\\OneDrive\\Desktop\\spring 2025\\app stats\\project\\GymDataAnalysis\\gym_members_exercise_tracking.csv")
-head(data)
+
+# Julian's path: "D:\\2school\\STAT430\\GymDataAnalysis\\gym_members_exercise_tracking.csv"
+# Peter's path:  "C:\\Users\\PGBiz\\OneDrive\\Desktop\\spring 2025\\app stats\\project\\GymDataAnalysis\\gym_members_exercise_tracking.csv"
+GymData = read.csv("D:\\2school\\STAT430\\GymDataAnalysis\\gym_members_exercise_tracking.csv")
+head(GymData)
+
