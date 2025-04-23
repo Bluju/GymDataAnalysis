fa4a82e79cd6292d662ce5d35c60bfab8ff45d90
peter-biz <pgbizoukas@gmail.com>
2025-04-22 20:34:19 -0500

    Test

--- FinalProjectGymData.R
+++ FinalProjectGymData.R
@@ -1,3 +1,5 @@
 ### Julian Sahagun and Peter Bizoukas
 ### STAT 43000
 ### Analysis of Gym Dataset (Final Project)
+data = read.csv("C:\\Users\\PGBiz\\OneDrive\\Desktop\\spring 2025\\app stats\\project\\GymDataAnalysis\\gym_members_exercise_tracking.csv")
+head(data)
