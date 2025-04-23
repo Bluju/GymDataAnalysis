023dea03aac74a787aed044aafd9f2e1ac74e51b
peter-biz <pgbizoukas@gmail.com>
2025-04-22 20:37:58 -0500

    removed fluff if we're being honest

--- FinalProjectGymData.R
+++ FinalProjectGymData.R
@@ -2,8 +2,7 @@
 ### STAT 43000
 ### Analysis of Gym Dataset (Final Project)
 
-# Julian's path: "D:\\2school\\STAT430\\GymDataAnalysis\\gym_members_exercise_tracking.csv"
-# Peter's path:  "C:\\Users\\PGBiz\\OneDrive\\Desktop\\spring 2025\\app stats\\project\\GymDataAnalysis\\gym_members_exercise_tracking.csv"
+# GymData = read.csv("C:\\Users\\PGBiz\\OneDrive\\Desktop\\spring 2025\\app stats\\project\\GymDataAnalysis\\gym_members_exercise_tracking.csv")
 GymData = read.csv("D:\\2school\\STAT430\\GymDataAnalysis\\gym_members_exercise_tracking.csv")
 head(GymData)
 
